e86be586fdec3422f0143e93bfda032070ce3bb5
lennart84 <lennartschleifer@gmail.com>
2016-09-06 12:37:10 +0200

    boilerplate update auf v1.1

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var jshint       = require('gulp-jshint');
 var sass         = require('gulp-sass');
 var concat       = require('gulp-concat');
 var uglify       = require('gulp-uglify');
+var plumber      = require('gulp-plumber');
 var rename       = require('gulp-rename');
 var htmlmin      = require('gulp-htmlmin');
 var sourcemaps   = require('gulp-sourcemaps');
@@ -15,6 +16,14 @@ var autoprefixer = require('autoprefixer');
 var pixrem       = require('pixrem');
 var cssnano      = require('cssnano');
 var browserSync  = require('browser-sync').create();
+
+
+// Copy folders
+gulp.task('copy-folders', function() {
+    return gulp
+        .src(['src/fonts/**/*'], {base: 'src/'})
+        .pipe(gulp.dest('dist/'));
+});
 
 
 // Lint JS-Files
@@ -32,12 +41,14 @@ gulp.task('scripts', function() {
         .pipe(concat('main.js'))
         .pipe(gulp.dest('dist/js'))
         .pipe(rename('main.min.js'))
+        .pipe(plumber())
         .pipe(uglify())
+        .pipe(plumber.stop())
         .pipe(gulp.dest('dist/js'));
 });
 
 // Compile Sass
-gulp.task('sass', ['bower'], function() {
+gulp.task('sass', function() {
     return gulp
         .src('src/scss/style.scss')
         .pipe(sourcemaps.init())
@@ -51,13 +62,13 @@ gulp.task('sass', ['bower'], function() {
 });
 
 // Wire Bower Dependencies into SCSS
-gulp.task('bower', function () {
+/*gulp.task('bower', function () {
     var wiredep = require('wiredep').stream;
     return gulp
         .src('src/scss/style.scss')
         .pipe(wiredep())
         .pipe(gulp.dest('src/scss/'));
-});
+});*/
 
 // Minify & Autoprefix CSS
 gulp.task('css', function () {
@@ -76,7 +87,7 @@ gulp.task('css', function () {
 });
 
 // Minify HTML Files
-gulp.task('minify', function() {
+gulp.task('htmlmin', function() {
     return gulp
         .src('src/*.html')
         .pipe(htmlmin({
@@ -97,7 +108,7 @@ gulp.task('imagemin', function() {
 // Watch Files For Changes
 gulp.task('watch', function() {
     browserSync.init({
-      proxy: 'localhost/gulp_boilerplate/dist/'
+      proxy: 'localhost/b-gulp/dist/'
     });
     gulp.watch('src/js/*.js', ['lint', 'scripts']);
     gulp.watch('src/scss/**/*.scss', ['sass', 'css']);
@@ -105,4 +116,4 @@ gulp.task('watch', function() {
 });
 
 // Default Tasks
-gulp.task('default', ['lint', 'sass', 'css', 'scripts', 'minify', 'imagemin', 'watch']);
+gulp.task('default', ['lint', 'sass', 'css', 'scripts', 'htmlmin', 'imagemin', 'copy-folders', 'watch']);
